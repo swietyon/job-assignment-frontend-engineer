fc03d8efc7db3a278eae24a18e5f5e4a46dae2fe
swietyon <maks.sw98@gmail.com>
2026-06-29 23:01:09 +0200

    test: add tests for ArticleList component with mock data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders conduit link", () => {
-  render(<App />);
-  const linkElement = screen.getAllByText(/conduit/i)[0];
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./components/Header", () => function MockHeader() { return <div>Header</div>; });
+jest.mock("./components/ArticleList", () => function MockArticleList() { return <div>ArticleList</div>; });
+jest.mock("./pages/LoginRegister", () => function MockLoginPage() { return <div>LoginPage</div>; });
+jest.mock("./pages/Logout", () => function MockLogoutPage() { return <div>LogoutPage</div>; });
+jest.mock("./pages/Profile", () => function MockProfilePage() { return <div>ProfilePage</div>; });
+jest.mock("./components/Article", () => function MockArticlePage() { return <div>ArticlePage</div>; });
+jest.mock("./components/Editor", () => function MockEditorPage() { return <div>EditorPage</div>; });
+
+describe("App", () => {
+  it("renders home page by default", () => {
+
+    window.location.hash = "#/";
+
+    render(<App />);
+
+    expect(screen.getByText("Header")).toBeInTheDocument();
+    expect(screen.getByText("ArticleList")).toBeInTheDocument();
+  });
 });
